9172cf99d1b54f5ca5e4ef4d2dc1f0cc7364baad
joselynroam-hue <joselynroam@gmail.com>
2026-08-06 18:23:54 +0200

    Create regalos.js

---
+++ regalos.js
@@ -0,0 +1,48 @@
+const regalos = [
+
+{
+id:1,
+nombre:"Carricoche",
+precio:"239 € aprox.",
+enlace:"",
+categoria:"Paseo",
+reservado:false
+},
+
+{
+id:2,
+nombre:"Maxi-Cosi",
+precio:"319 € aprox.",
+enlace:"",
+categoria:"Paseo",
+reservado:false
+},
+
+{
+id:3,
+nombre:"Colecho",
+precio:"89 € aprox.",
+enlace:"",
+categoria:"Descanso",
+reservado:false
+},
+
+{
+id:4,
+nombre:"Colchón colecho",
+precio:"25 € aprox.",
+enlace:"",
+categoria:"Descanso",
+reservado:false
+},
+
+{
+id:5,
+nombre:"Protector impermeable",
+precio:"15 € aprox.",
+enlace:"",
+categoria:"Descanso",
+reservado:false
+}
+
+];
